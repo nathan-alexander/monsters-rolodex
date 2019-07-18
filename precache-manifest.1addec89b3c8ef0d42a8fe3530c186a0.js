7d12a97c14c122ca84e005db1fd685cdb4649fd6
Nathan Alexander <hatenate@gmail.com>
2019-07-18 09:29:28 -0700

    Updates

---
+++ precache-manifest.1addec89b3c8ef0d42a8fe3530c186a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae37b5320454817b92e8c4b000ecbdb2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ac5105bd69e66db9fc69",
+    "url": "/monsters-rolodex/static/css/main.993839bf.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ac5105bd69e66db9fc69",
+    "url": "/monsters-rolodex/static/js/main.f1b4acd7.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
